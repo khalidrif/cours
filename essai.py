dc04489955435b5cae498472b155a39b2003db30
khalidrif <khalid.rhif@gmail.com>
2026-03-17 03:29:49 -0400

    Update essai.py

--- essai.py
+++ essai.py
@@ -1,7 +1,14 @@
 import streamlit as st
+import time
 
-st.title("🔍 Test Streamlit")
-name = st.text_input("Quel est ton nom ?")
-if st.button("Dire bonjour"):
-    st.write(f"Salut {name} 👋")
+st.title("🧪 Simulation XRP Coinbase")
 
+# Variables test
+mid = 1.02
+usdc = 100.0
+xrp = 50.0
+
+st.metric("💵 USDC libres", f"{usdc:.2f}$")
+st.metric("💠 XRP libres", f"{xrp:.2f}")
+st.metric("📊 Prix XRP", f"{mid:.5f}")
+st.caption(f"Dernière mise à jour : {time.strftime('%H:%M:%S')}")
