9bf183262800db89eb1de167bfc4f7de02a07b29
khalidrif <khalid.rhif@gmail.com>
2026-03-18 00:58:14 -0400

    Update essai.py

--- essai.py
+++ essai.py
@@ -5,17 +5,3 @@ import os
 import time
 from streamlit_autorefresh import st_autorefresh
 
-import streamlit as st
-from coinbase.rest import RESTClient
-
-st.title("Mon Solde XRP")
-   accounts = client.get_accounts()
-    
-    # Recherche du XRP
-    xrp_balance = next((acc['available_balance']['value'] for acc in accounts['accounts'] if acc['currency'] == 'XRP'), "0.00")
-    
-    # Affichage géant du solde
-    st.metric(label="XRP disponible", value=f"{xrp_balance} XRP")
-
-except Exception as e:
-    st.error(f"Erreur : {e}")
